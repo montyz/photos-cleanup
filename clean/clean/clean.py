258b413cd884e9a6832e7bde04a16f3ac40e14b0
montyz <monty@codetransform.com>
2020-06-20 15:18:07 -0700

    got tagging to work

--- clean/clean/clean.py
+++ clean/clean/clean.py
@@ -1,11 +1,38 @@
 import csv
 import sys
 
+from subprocess import Popen, PIPE, STDOUT
+
+replace_keyword_script = """
+on run {photoid, keyword}
+	tell application "Photos"
+		set thisItem to media item id photoid
+		set keywords of thisItem to {keyword}
+	end tell
+end run
+"""
+
+
+def run_applescript(script, arg_list):
+    p = Popen(['osascript', '-'] + arg_list, text=True, stdin=PIPE, stdout=PIPE, stderr=STDOUT)
+    stdout, stderr = p.communicate(script)
+    return p.returncode, stdout, stderr
+
+
 def main(argv):
-    with open(argv[1]) as csvfile:
-        csvreader = csv.reader(csvfile)
-        for row in csvreader:
-            print(row[0])
+    with open(argv[1]) as inputfile:
+        with open("errors.csv", "w") as errorfile:
+            inputcsv = csv.DictReader(inputfile)
+            errorcsv = csv.DictWriter(errorfile, ["uuid", "error"])
+            errorcsv.writeheader()
+            for d in inputcsv:
+                returncode, stdout, stderr = run_applescript(replace_keyword_script, [d['uuid'], 'deleteme'])
+                if returncode != 0:
+                    print("{} returned error code: {}".format(d['uuid'], returncode))
+                    errorcsv.writerow({'uuid': d['uuid'], 'error': stdout.strip()})
+                else:
+                    print("{} succeeded".format(d['uuid']))
+
 
 if __name__ == "__main__":
     # execute only if run as a script
